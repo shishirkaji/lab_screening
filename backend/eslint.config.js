0a49f6e7781b233fc5ff39497ece2f2a4e355063
Shishir <shishirkaji@gmail.com>
2025-05-13 18:04:29 +1000

    Initial set up

---
+++ backend/eslint.config.js
@@ -0,0 +1,19 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import perfectionist from "eslint-plugin-perfectionist";
+export default tseslint.config(
+  {
+    ignores: ["**/*.js"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  perfectionist.configs["recommended-natural"]
+);
---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+let i;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
